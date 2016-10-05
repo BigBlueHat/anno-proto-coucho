f52f024d6db4ccc478b1c4372c6d0473a08ea23d
BigBlueHat <byoung@bigbluehat.com>
2016-10-05 15:14:17 -0400

    Remove CouchDB _revisions content from response
    Not harmful to Web Annotation, but not helpful neither

--- _design/anno/shows/add_context.js
+++ _design/anno/shows/add_context.js
@@ -4,6 +4,8 @@ function(doc, req) {
   // JSON-LD
   var rv = doc;
   rv['@context'] = "http://www.w3.org/ns/anno.jsonld";
+  // remove CouchDB _revisions cruft (since this isn't the CouchDB API)
+  delete rv['_revisions'];
   return {
     headers: {
       'Allow': 'GET, HEAD, OPTIONS, PUT, DELETE',
